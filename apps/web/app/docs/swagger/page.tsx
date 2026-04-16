1751f0c550553e0d6d4ffe286d0d244f967c9af4
liuxing.li <liuxing.li@npt.sg>
2026-04-16 11:25:23 +0800

    Fix docs iframe redirect loop by using static HTML filenames

--- apps/web/app/docs/swagger/page.tsx
+++ apps/web/app/docs/swagger/page.tsx
@@ -2,7 +2,7 @@ export default function SwaggerPage() {
   return (
     <iframe
       title="Swagger UI"
-      src="/docs/swagger.html"
+      src="/docs/swagger-static.html"
       style={{ border: 0, width: "100%", minHeight: "100vh" }}
     />
   );
